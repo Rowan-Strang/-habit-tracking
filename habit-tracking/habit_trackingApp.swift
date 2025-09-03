982c7999f7eb4dab96b1d78525c3e4a829792bab
Rowan Strang <rowanstrang@gmail.com>
2025-09-03 12:05:36 +1200

    Initial Commit

---
+++ habit-tracking/habit_trackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  habit_trackingApp.swift
+//  habit-tracking
+//
+//  Created by MacRow on 03/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct habit_trackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
